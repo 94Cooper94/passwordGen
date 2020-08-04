7007f6c653adf05d5bd6a106f1b68abd94f9b5df
Cooper <eliteswimmer42@gmail.com>
2020-08-04 11:16:07 -0500

    Went through and tried to simplify some lines. There are a few more elements that I hope to clean-up prior to turning in. I plan on adding a few more comments to make the code more understandable. Ultimately, I hope to add images and a thorough readme.md

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // This is where the behavior of the password generator begins.
-// Choose between 8-128 characters.
-// 128 is a lot of gravy, but we dont ask questions.
 
+// Here, we give life to the 'generate' button and tie it to our function
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Here we're declaring arrays with each possible option per user preference
+// Below we declare arrays with each possible option per user preference
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "}", "]", "|", ":", ";", "'", ",", "<", ".", ">", "/", "?"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -31,18 +30,18 @@ function passwordGenerator() {
     alert(`Your password will be ${clickLength} characters`);
   
   // Get user input on other password specifications
-  var clickNumber = confirm("Click OK if you want numbers in your password");
-  var clickSpecial = confirm("Click OK if you want special characters in your password");
-  var clickLower = confirm("Click OK if you want lower case letters in your password");
-  var clickUpper = confirm("Click OK if you want upper case letters in your password");
+  var cNumber = confirm("Click OK if you want numbers in your password");
+  var cSpecial = confirm("Click OK if you want special characters in your password");
+  var cLower = confirm("Click OK if you want lower case letters in your password");
+  var cUpper = confirm("Click OK if you want upper case letters in your password");
     
     // Loop in case the user doesn't follow directions
-    while(clickNumber === false && clickSpecial === false && clickLower === false && clickUpper === false) {
+    while(cNumber === false && cSpecial === false && cLower === false && cUpper === false) {
       alert("You must at least select one option");
-      var clickNumber = confirm("Click OK if you want numbers in your password");
-      var clickSpecial = confirm("Click OK if you want special characters in your password");
-      var clickLower = confirm("Click OK if you want lower case letters in your password");
-      var clickUpper = confirm("Click OK if you want upper case letters in your password");
+      var cNumber = confirm("Click OK if you want numbers in your password");
+      var cSpecial = confirm("Click OK if you want special characters in your password");
+      var cLower = confirm("Click OK if you want lower case letters in your password");
+      var cUpper = confirm("Click OK if you want upper case letters in your password");
   }
 
     // Assign action to the password parameters we set
@@ -85,3 +84,8 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// Overall referenced a repository found on Github belonging to Tiffany Casey
+// I understand the premise and am utilizing external resources
+// to better understand how to think and formulate thoughts logically,
+// like a computer.
