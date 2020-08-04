6f4287a8b5f63dd83eedcf9772546c998c71fd20
Cooper <eliteswimmer42@gmail.com>
2020-08-04 12:17:28 -0500

    Attempting to clean up the code/syntax. Ideally, I'll use fewer lines, fewer variable/array declarations. I'd like to add a detailed readme still, but that is looking like a much later step in the process. First, I'd like to make sure the code is clean and functions well.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// This is where the behavior of the password generator begins.
-
-// Here, we give life to the 'generate' button and tie it to our function
-document.querySelector("#generate").addEventListener("click", writePassword);
+// Assignment Code
+document.querySelector("#generate").addEventListener("click",passwordGenerator);
 
 // Below we declare arrays with each possible option per user preference
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -9,25 +7,27 @@ var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", 
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Now we must declare the variables
-var clickLength = "";
-var clickNumber;
-var clickSpecial;
-var clickLower;
-var clickUpper;
+// We declare variables for user-inputted length and whether or not the user wants
+// numbers, uppercase, lowercase, or special characters.
+var length = "";
+var cNumber;
+var cSpecial;
+var cLower;
+var cUpper;
 
-// Declaring the function and prompting user on password length
+// We've established our content in the arrays, we've declared variables
+// Time for user input
 function passwordGenerator() {
-  var clickLength = (prompt("Choose a password length between 8 and 128 characters"));
+  var length = (prompt("Choose a password length between 8 and 128 characters"));
 
   // If the user chooses an invalid length, this while loop will activate
-  while(clickLength <= 7 || clickLength >= 129){
+  while(length <= 7 || length >= 129){
     alert("Password must be between 8 and 128 characters! Please try again.")
-    var clickLength = (prompt("Choose a password length between 8 and 128 characters"));
+    var length = (prompt("Choose a password length between 8 and 128 characters"));
     }
 
     // Let user know how many characters they have
-    alert(`Your password will be ${clickLength} characters`);
+    alert(`Your password will be ${length} characters`);
   
   // Get user input on other password specifications
   var cNumber = confirm("Click OK if you want numbers in your password");
@@ -35,57 +35,34 @@ function passwordGenerator() {
   var cLower = confirm("Click OK if you want lower case letters in your password");
   var cUpper = confirm("Click OK if you want upper case letters in your password");
     
-    // Loop in case the user doesn't follow directions
-    while(cNumber === false && cSpecial === false && cLower === false && cUpper === false) {
-      alert("You must at least select one option");
-      var cNumber = confirm("Click OK if you want numbers in your password");
-      var cSpecial = confirm("Click OK if you want special characters in your password");
-      var cLower = confirm("Click OK if you want lower case letters in your password");
-      var cUpper = confirm("Click OK if you want upper case letters in your password");
+  // Loop in case the user doesn't follow directions
+  while(cNumber === false && cSpecial === false && cLower === false && cUpper === false) {
+    alert("You must at least select one option");
+    var cNumber = confirm("Click OK if you want numbers in your password");
+    var cSpecial = confirm("Click OK if you want special characters in your password");
+    var cLower = confirm("Click OK if you want lower case letters in your password");
+    var cUpper = confirm("Click OK if you want upper case letters in your password");
+  }
+  var confirmCharacters = []
+
+  function concat(confirm, arr) {
+    if (confirm) {
+      confirmCharacters = confirmCharacters.concat(arr);
+    }
   }
 
-    // Assign action to the password parameters we set
-    var confirmCharacters = []
-  
-  if (clickNumber) {
-    confirmCharacters = confirmCharacters.concat(number)
-  }
+concat(cNumber, number);
+concat(cSpecial, special);
+concat(cLower, lower);
+concat(cUpper, upper);
 
-  if (clickSpecial) {
-    confirmCharacters = confirmCharacters.concat(special)
-  }
+// Empty string set forth to be filled with our random array
+var rngPassword = ""
 
-  if (clickLower) {
-    confirmCharacters = confirmCharacters.concat(lower)
-  }
+// The loop that cycles through each position in the array and applies math functions to randomize each value.
+for (var i = 0; i < length; i++) {
+  rngPassword = rngPassword + confirmCharacters[Math.floor(Math.random() * confirmCharacters.length)];
+}var passwordText = document.querySelector("#password");
 
-  if (clickUpper) {
-    confirmCharacters = confirmCharacters.concat(upper)
-  }
-    
-    // Store our info
-    console.log(confirmCharacters)
-
-  // Empty string set forth to be filled with our random array
-  var rngPassword = ""
-
-  // The loop that cycles through each position in the array and applies math functions to randomize each value.
-  for (var i = 0; i < clickLength; i++) {
-    rngPassword = rngPassword + confirmCharacters[Math.floor(Math.random() * confirmCharacters.length)];
-    console.log(rngPassword)
-  }
-  return rngPassword;
+passwordText.value = rngPassword;
 }
-
-// This displays the password that we generate
-function writePassword() {
-  var password = passwordGenerator();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Overall referenced a repository found on Github belonging to Tiffany Casey
-// I understand the premise and am utilizing external resources
-// to better understand how to think and formulate thoughts logically,
-// like a computer.
